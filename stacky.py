69186e6be70ab1edc77b9400a6d896eed2b38706
Thomas Maddox <thomas.maddox@RACKSPACE.com>
2013-04-30 08:52:44 -0500

    added indent to JSON report dump

--- stacky.py
+++ stacky.py
@@ -364,8 +364,8 @@ if __name__ == '__main__':
 
         metadata = r[0]
         report = r[1:]
-        if metadata['format'] == 'json':
-            print(json.dumps(report))
+        if metadata['report_format'] == 'json':
+            print(json.dumps(report), indent=4)
         else:
             metadata_report = [['Key', 'Value']]
             for k, v in metadata.iteritems():
